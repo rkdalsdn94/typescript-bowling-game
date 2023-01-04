ba4a85b38b3c122c8f6a56460f3338f8aa02157d
minwooKang <rkdalsdn94@gmail.com>
2023-01-05 01:45:44 +0900

    refactor: 게임 객체를 매 테스트마다 생성하는 중복 제거
     - jest에서 제공하는 beforeEach를 활용하여 게임 객체를 생성하는 중복 제거

--- src/test/game.test.ts
+++ src/test/game.test.ts
@@ -1,13 +1,16 @@
 import Game from '../main/game.main';
 
 describe('게임 테스트', () => {
+  let game: Game;
+  beforeEach(() => {
+    game = new Game();
+  });
+
   test('Game 객체가 생성된다면 통과', () => {
-    const game: Game = new Game();
     expect(game).toStrictEqual(new Game());
   });
 
   test('Game에서 공을 굴릴 수 있으면 통과', () => {
-    const game = new Game();
     game.roll(0);
   });
 });
